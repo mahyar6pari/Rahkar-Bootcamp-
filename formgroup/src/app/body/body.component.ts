5ac4ec4d720ec06634d82abaf121aa5c21cb9bd4
mahyar6pari <mahyarsheshpari4@gmail.com>
2023-11-23 20:32:44 +0330

    AngularFormGroup

---
+++ formgroup/src/app/body/body.component.ts
@@ -0,0 +1,15 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-body',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './body.component.html',
+  styleUrl: './body.component.scss'
+})
+export class BodyComponent {
+@Input() lname!:string;
+color:string="red"
+  
+}
